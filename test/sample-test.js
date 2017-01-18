1327352f97408b1751abdff92933b3e8fe99cb7e
Nattawut <nattawut@thinknet.co.th>
2017-01-19 01:04:26 +0700

    add webpack testing

---
+++ test/sample-test.js
@@ -0,0 +1,13 @@
+var expect = require('chai').expect;
+
+describe('Sample tests', function() {
+  it('Should pass', function() {
+    expect(true).to.be.true;
+  });
+});
+
+describe('Sample tests with Arrow =>', () => {
+  it('Should pass', function () {
+    expect(true).to.be.true;
+  });
+});
---
+++ webpack-test.config.js
@@ -0,0 +1,27 @@
+var webpack = require('webpack');
+var nodeExternals = require('webpack-node-externals');
+var WebpackShellPlugin = require('webpack-shell-plugin');
+
+
+var config = {
+  entry: './all-tests.js',
+  output: {
+    filename: 'testBundle.js'
+  },
+  target: 'node',
+  externals: [nodeExternals()],
+  node: {
+    fs: 'empty'
+  },
+
+
+  plugins: [
+    new WebpackShellPlugin({
+      onBuildExit: "NODE_ENV=test ./node_modules/.bin/istanbul cover ./node_modules/.bin/_mocha --colors test"
+      // onBuildExit: "mocha --colors --require spec-helper.js testBundle.js"
+    })
+  ]
+};
+
+
+module.exports = config;
